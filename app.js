be5a5d68867ef21e293ed6442a66b9820c6d54f6
umarahh4u <umarahh4u@gmail.com>
2024-07-09 12:08:01 +0100

    updates

--- app.js
+++ app.js
@@ -54,28 +54,16 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-// app.use("/", (req, res) => {
-//   const { query, pathname } = url.parse(req.url, true);
 
-//   if (pathname === "/") {
-//     res.writeHead(200, {
-//       "Content-type": "application/json",
-//     });
-
-//     res.end({
-//       statusCode: 200,
-//       message: "Welcome to the User Authentication and Organisation API",
-//       endpoints: `{
-//             /auth/register: 	"POST - Register a new user",
-//             /auth/login: 	"POST - Log in a user",
-//             /api/users/:id:	"GET - Get user details [PROTECTED]",
-//             /api/organisations: 	"POST - Create a new organisation [PROTECTED]",
-//             /api/organisations/:orgId: 	"GET - Get a single organisation record [PROTECTED]",
-//             /api/organisations/:orgId/users: 	"POST - Add a user to an organisation [PROTECTED]",
-//       }`,
-//     });
-//   }
-// });
+app.use("/", (req, res) => {
+  res.json({
+    statusCode: 200,
+    message: "Welcome to the User Authentication and Organisation API",
+    endpoints: JSON.parse(
+      '{ "/auth/register": "POST - Register a new user","/auth/login": "POST - Log in a user","/api/users/:id": "GET - Get user details [PROTECTED]","/api/organisations": "POST - Create a new organisation [PROTECTED]", "/api/organisations/:orgId":"GET - Get a single organisation record [PROTECTED]","/api/organisations/:orgId/users":"POST - Add a user to an organisation [PROTECTED]"}'
+    ),
+  });
+});
 
 app.use("/", userRouter);
 app.use("/api/organisations", organisationRouter);
